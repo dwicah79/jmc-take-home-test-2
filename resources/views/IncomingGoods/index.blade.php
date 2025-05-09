1692699adb9ee1ec0de7b586b4fe37e2cba4774d
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 22:53:44 +0700

    test view incomingGoods

---
+++ resources/views/IncomingGoods/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('content')
+    <div class="flex flex-col items-center justify-center h-screen w-full bg-white">
+        <h1 class="text-2xl font-bold mb-4">Ini barang masuk</h1>
+        <p class="text-gray-600">Selamat datang di halaman dashboard!</p>
+    </div>
+@endsection
