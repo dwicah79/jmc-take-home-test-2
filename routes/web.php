4102ee566428ca262878784b901d37b1e5d4e52b
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 22:54:12 +0700

    create route for IncomingGoods

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IncomingGoodsController;
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'index'])->name('login');
@@ -13,4 +14,12 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::prefix('incoming-goods')->group(function () {
+        Route::get('/', [IncomingGoodsController::class, 'index'])->name('incoming-goods.index');
+        Route::post('/store', [IncomingGoodsController::class, 'store'])->name('incoming-goods.store');
+        Route::get('/{id}/edit', [IncomingGoodsController::class, 'edit'])->name('incoming-goods.edit');
+        Route::put('/{id}', [IncomingGoodsController::class, 'update'])->name('incoming-goods.update');
+        Route::delete('/{id}', [IncomingGoodsController::class, 'destroy'])->name('incoming-goods.destroy');
+    });
 });
