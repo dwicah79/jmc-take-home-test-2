19af6dcc804f74846f96527eaae96fe32ddd5109
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 17:27:17 +0700

    create testing view dashboard

---
+++ resources/views/Dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+@section('content')
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+        <p class="text-gray-600">Selamat datang di halaman dashboard!</p>
+        <img src="{{ asset('images/Warehousing-101.jpg') }}" alt="Dashboard Illustration" class="mt-4 w-1/2">
+    </div>
+@endsection
