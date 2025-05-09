effd8cb234c213265437f33053c3926ff9c48042
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 03:27:59 +0700

    add custom message validation update category

---
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name_category' => 'required|string|max:255',
+            'code_category' => 'required|string|max:255|unique:categories,code_category,' . $this->route('category')->id,
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name_category.required' => 'Nama kategori harus diisi',
+            'name_category.max' => 'Nama kategori maksimal 255 karakter',
+            'code_category.required' => 'Kode kategori harus diisi',
+            'code_category.max' => 'Kode kategori maksimal 255 karakter',
+            'code_category.unique' => 'Kode kategori sudah terdaftar, silakan gunakan kode lain',
+        ];
+    }
+}
