48f0e55e889022da4382a2d3f64234e2c959afe0
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 22:20:32 +0700

    test template dashboard

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('content')
-    <div class="flex flex-col items-center justify-center h-screen">
+    <div class="flex flex-col items-center justify-center h-screen w-full bg-white">
         <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
         <p class="text-gray-600">Selamat datang di halaman dashboard!</p>
         <img src="{{ asset('images/Warehousing-101.jpg') }}" alt="Dashboard Illustration" class="mt-4 w-1/2">
