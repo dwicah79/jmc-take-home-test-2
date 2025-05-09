1c11955d578c58a5471410547d9ba5b4bb697b5d
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 00:45:49 +0700

    delete h-full in index and add padding in paginator

--- resources/views/Category/index.blade.php
+++ resources/views/Category/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <x-breadcrumb :items="[['label' => 'Kategori Barang', 'url' => route('categories.index')]]" />
 
-    <div class=" h-full w-full bg-white">
+    <div class="  w-full bg-white">
         <div class="md:p-10">
             <x-search-component buttonText="+ Tambah Barang">
                 <input type="text" class="border rounded px-3 py-2 text-sm" placeholder="Cari data...">
@@ -14,15 +14,17 @@
                     <td class="px-4 py-2">{{ ($categories->currentPage() - 1) * $categories->perPage() + $loop->iteration }}
                     </td>
                     <td class="px-4 py-2">
-                        <button class="text-blue-600 hover:underline"><i class="fa-solid fa-pencil"></i></button>
-                        <button class="text-red-600 hover:underline"><i class="fa-solid fa-trash-can"></i></button>
+                        <div class="inline-flex space-x-4">
+                            <button class="text-blue-600 hover:underline"><i class="fa-solid fa-pencil"></i></button>
+                            <button class="text-red-600 hover:underline"><i class="fa-solid fa-trash-can"></i></button>
+                        </div>
                     </td>
                     <td class="px-4 py-2 font-semibold">{{ $category->code_category }}</td>
                     <td class="px-4 py-2 font-semibold">{{ $category->name_category }}</td>
                 </tr>
             @endforeach
         </x-data-table>
-        <div class="mt-5 px-5">
+        <div class=" p-5">
             {{ $categories->links() }}
         </div>
     </div>
