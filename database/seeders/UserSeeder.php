57d446a03f4d7983384daea0067f05706d4af442
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 14:30:29 +0700

    initial project

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $operatorRole = Role::firstOrCreate(['name' => 'operator']);
+
+        $admin = User::create([
+            'name' => 'Administrator',
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'is_locked' => false,
+        ]);
+        $admin->assignRole($adminRole);
+        $operator = User::create([
+            'name' => 'Operator User',
+            'username' => 'operator',
+            'email' => 'operator@gmail.com',
+            'password' => Hash::make('password'),
+            'is_locked' => false,
+        ]);
+        $operator->assignRole($operatorRole);
+    }
+}
