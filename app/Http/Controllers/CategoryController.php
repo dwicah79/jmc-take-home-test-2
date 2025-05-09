da9599e3602bd9030eb7e9f9d82392de5449c7f3
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 00:33:48 +0700

    create category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service\CategoryService;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    protected $categoryService;
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+    public function index(Request $request)
+    {
+        $categories = $this->categoryService->getAll();
+        return view('Category.index', compact('categories'));
+    }
+}
