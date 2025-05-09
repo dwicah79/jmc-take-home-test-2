3398902c9de8421af0efc00f673359224e7c055f
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 23:01:12 +0700

    implement breadcrumb to index incominggoods

--- resources/views/IncomingGoods/index.blade.php
+++ resources/views/IncomingGoods/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.app')
 @section('content')
+    <x-breadcrumb :items="[['label' => 'Barang Masuk', 'url' => route('incoming-goods.index')]]" />
+
     <div class="flex flex-col items-center justify-center h-screen w-full bg-white">
         <h1 class="text-2xl font-bold mb-4">Ini barang masuk</h1>
         <p class="text-gray-600">Selamat datang di halaman dashboard!</p>
