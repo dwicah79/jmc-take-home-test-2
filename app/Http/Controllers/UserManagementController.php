c87f2334d64c2fa419931a621fab3b33fda02263
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 15:14:28 +0700

    create function index user management controller

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\UserManagementService;
 use Illuminate\Http\Request;
 
 class UserManagementController extends Controller
 {
-    //
+    protected $userService;
+
+    public function __construct(UserManagementService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+        $users = $this->userService->all($search);
+        return $users;
+        return view('UserManagement.index', compact('users'));
+    }
 }
