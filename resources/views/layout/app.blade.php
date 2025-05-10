293b2f20bcc3daea931212d9d46b6c26fa22f1b7
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 14:19:40 +0700

    import cleave js library with cdn to masking number

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/cleave.js@1.6.0/dist/cleave.min.js"></script>
 
     <style>
         #sidebar {
