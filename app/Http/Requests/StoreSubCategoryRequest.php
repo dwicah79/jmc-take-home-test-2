e3df086bba447961e07194f271dbd0384d8d6be0
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 13:45:33 +0700

    create custom validator request sub category store

---
+++ app/Http/Requests/StoreSubCategoryRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSubCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sub_category_name' => 'required',
+            'price_range' => 'required|numeric',
+            'category_id' => 'required|exists:categories,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'sub_category_name.required' => 'Nama sub kategori harus diisi',
+            'price_range.required' => 'Rentang harga harus diisi',
+            'price_range.numeric' => 'Rentang harga harus berupa angka',
+            'category_id.required' => 'Kategori harus dipilih',
+            'category_id.exists' => 'Kategori yang dipilih tidak valid',
+        ];
+    }
+}
