2c4fce441b35b86904fc601309a5ec7a5ca4586e
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 16:04:47 +0700

    add function lock and unlock in user controller

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -20,4 +20,28 @@ class UserManagementController extends Controller
         $users = $this->userService->all($search);
         return view('UserManagement.index', compact('users'));
     }
+
+    public function lock($id)
+    {
+        try {
+            $this->userService->lock($id);
+            return redirect()->route('user.index')
+                ->with('success', 'User berhasil dikunci');
+        } catch (\Exception $e) {
+            return redirect()->route('user.index')
+                ->with('error', 'Gagal mengunci user: ' . $e->getMessage());
+        }
+    }
+
+    public function unlock($id)
+    {
+        try {
+            $this->userService->unlock($id);
+            return redirect()->route('user.index')
+                ->with('success', 'User berhasil dibuka kuncinya');
+        } catch (\Exception $e) {
+            return redirect()->route('user.index')
+                ->with('error', 'Gagal membuka kunci user: ' . $e->getMessage());
+        }
+    }
 }
