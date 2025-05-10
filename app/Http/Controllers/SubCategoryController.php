6951533ca1e8576f201e96902989b7b3e419cb4a
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-10 14:10:27 +0700

    add category to sub category index

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreSubCategoryRequest;
+use App\Models\Category;
 use App\Service\SubCategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SubCategoryController extends Controller
 {
@@ -15,7 +18,23 @@ class SubCategoryController extends Controller
     public function index(Request $request)
     {
         $search = $request->input('search');
+        $category = Category::all();
         $subcategories = $this->subCategoryService->getAll($search);
-        return view('SubCategory.index', compact('subcategories'));
+        return view('SubCategory.index', compact('subcategories', 'category'));
+    }
+
+    public function store(StoreSubCategoryRequest $request)
+    {
+        dd($request->all());
+        $data = $request->validated();
+        try {
+            $this->subCategoryService->store($data);
+            return redirect()->route('subcategories.index')
+                ->with('success', 'Subkategori berhasil ditambahkan');
+        } catch (ValidationException $e) {
+            return redirect()->back()
+                ->withErrors($e->validator)
+                ->withInput();
+        }
     }
 }
