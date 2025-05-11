c75fefe5d95a433df456937c3bd9bf2f75e9bfec
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-12 04:41:42 +0700

    hotfix: add function verified

--- app/Repository/Interfaces/IncomingGoodsDetileRepositoryinterfaces.php
+++ app/Repository/Interfaces/IncomingGoodsDetileRepositoryinterfaces.php
@@ -5,4 +5,5 @@ interface IncomingGoodsDetileRepositoryinterfaces
 {
     public function insert(array $items);
     public function deleteByincomingGoodsId($incomingGoodsId);
+    public function verified($id, $data);
 }
