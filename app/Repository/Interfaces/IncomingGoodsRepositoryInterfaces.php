b9d188e8c7dcf218ea8da9c4a7301d36a278c77a
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-12 16:53:24 +0700

    add params search

--- app/Repository/Interfaces/IncomingGoodsRepositoryInterfaces.php
+++ app/Repository/Interfaces/IncomingGoodsRepositoryInterfaces.php
@@ -3,7 +3,7 @@ namespace App\Repository\Interfaces;
 
 interface IncomingGoodsRepositoryInterfaces
 {
-    public function all($filters);
+    public function all($filters, $search = null);
     public function find($id);
     public function create(array $data);
     public function update($id, array $data);
