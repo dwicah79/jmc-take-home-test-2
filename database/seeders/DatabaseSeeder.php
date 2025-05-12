a30cfeaa58c950275415287922407fef35a4a2b8
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-12 15:56:31 +0700

    comment seeder incoming goopds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategorySeeder::class,
             SubCategorySeeder::class,
-            IncomingGoodsSeeder::class,
+            // IncomingGoodsSeeder::class,
         ]);
     }
 }
